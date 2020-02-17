9c3a69b5806de34bc59649ccf903d90531d72bbd
Nhan Vi Nam <nhanvinam99@gmail.com>
2020-02-17 12:17:49 +0700

    first commit

---
+++ src/Components/HomeComponent/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+const prefix = "home";
+const HomeComponent = () => {
+  return (
+    <div className={prefix}>
+      <h3 className={`${prefix}__title`}>Hello </h3>
+    </div>
+  );
+};
+
+export default HomeComponent;
