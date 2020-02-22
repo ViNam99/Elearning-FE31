69dcf977bfe02e9629a91e05d6f18080ca6b4e43
Duy <duy@Duys-MacBook-Pro.local>
2020-02-22 11:51:59 +0700

    Footer

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import "./assets/styles/main.scss";
 
 import HomeComponent from "./Components/HomeComponent";
+import Footer from "./Components/Footer/Footer";
 const App = () => {
   return (
     <div>
       <HomeComponent />
+      <Footer/>
     </div>
   );
 };
