1481947fd50feb3df625426f220ce568ad57333f
Duy <duy@Duys-MacBook-Pro.local>
2020-02-23 23:49:56 +0700

    update about us

---
+++ src/Components/AboutUsComponent/LinktoContact/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Background from '../../../assets/img/inner-banner.jpg';
+const prefix="banner";
+const sectionStyle = {
+    textAlign: "center",
+    backgroundSize: "cover",
+    backgroundImage: `url(${Background})`
+    
+};
+const LinktoContact = () => {
+    return (
+            <div className={prefix} style={sectionStyle}>
+            <div className={`${prefix}__opacity`}>
+            <h6>Over 20 years of experience we’ll ensure you get the best guidance <p>Best service with the best quality works.</p></h6>
+            <button className="btn btn-primary contactus"><a href="">Contact Us</a></button>
+            <button className="btn getfreecourse"><a href="">Get Free Course</a></button>
+            </div>
+        </div>
+    );
+};
+
+export default LinktoContact;
--- src/Components/AboutUsComponent/index.js
+++ src/Components/AboutUsComponent/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Breadcrumb from './Breadcrums';
 import FeatureCourse from './FeatureCourse';
 import CountUpStudent from './CountUpStudent';
+import LinktoContact from './LinktoContact';
+import CountRequest from './CourseRequest';
 
 const MainAbout = () => {
     return (
@@ -9,6 +11,8 @@ const MainAbout = () => {
             <Breadcrumb/>
             <FeatureCourse/>
             <CountUpStudent/>
+            <LinktoContact/>
+            <CountRequest/>
         </div>
     );
 };
