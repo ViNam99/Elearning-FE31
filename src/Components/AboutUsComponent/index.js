cbabdff00ccc8f6cb2c23b4e819eb55a555b0490
Duy <duy@Duys-MacBook-Pro.local>
2020-02-23 21:51:00 +0700

    update about

--- src/Components/AboutUsComponent/index.js
+++ src/Components/AboutUsComponent/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Breadcrumb from './Breadcrums';
 import FeatureCourse from './FeatureCourse';
+import CountUpStudent from './CountUpStudent';
 
 const MainAbout = () => {
     return (
         <div>
             <Breadcrumb/>
             <FeatureCourse/>
+            <CountUpStudent/>
         </div>
     );
 };
