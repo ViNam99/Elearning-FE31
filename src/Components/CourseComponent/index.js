bfef1152798f88760035b6e77e960678c725163b
Nhan Vi Nam <nhanvinam99@gmail.com>
2020-02-28 10:54:38 +0700

    add classPrefixor

--- src/Components/CourseComponent/index.js
+++ src/Components/CourseComponent/index.js
@@ -2,22 +2,25 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBookOpen } from "@fortawesome/free-solid-svg-icons";
+import { classPrefixor } from "../../Utils/classPrefixor";
 const CourseComponent = () => {
   const prefix = "course";
+  const c = classPrefixor(prefix);
   return (
     <section className={prefix}>
       <Container>
-        <div className={`${prefix}__content text-center`}>
+        <div className={c`content `}>
           <h2>OUR EDUCATION COURSES</h2>
           <FontAwesomeIcon
             icon={faBookOpen}
-            className={`${prefix}__content__icon`}
+            className={c`content__icon`}
           />
           <p>
             From concept through construction, to the finishing touches of each
             of your projects, you can rely on the our team to deliver a personal
             environment tailored specifically for you.
           </p>
+          
         </div>
       </Container>
     </section>
