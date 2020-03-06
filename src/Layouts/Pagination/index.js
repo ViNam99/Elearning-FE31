fa2d9d9b397f25230785f8d6e770a93a58e00b0a
Nhan Vi Nam <nhanvinam99@gmail.com>
2020-03-06 14:12:41 +0700

    fix prev and next pagination

--- src/Layouts/Pagination/index.js
+++ src/Layouts/Pagination/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { Pagination } from "react-bootstrap";
+import { useDispatch } from "react-redux";
+import { createAction } from "../../redux/Actions";
+import { CHANGE_PAGE_TYPE } from "../../redux/Constants";
 const PaginationComponent = ({ ...props }) => {
+  const dispatch = useDispatch();
   const { totalCount, handleChangePage, pageSize } = props;
   const totalPage = Math.ceil(totalCount / pageSize);
   //   Ví dụ totalPage = 5 => [0, 1, 2, 3, 4]
@@ -8,17 +12,24 @@ const PaginationComponent = ({ ...props }) => {
   const renderPage = () => {
     return pages.map((page, index) => {
       return (
-        <Pagination.Item key={index} onClick={() => handleChangePage(page + 1)}>
+        <Pagination.Item
+          key={index}
+          onClick={() => handleChangePage(page + 1)}
+        >
           {page + 1}
         </Pagination.Item>
       );
     });
   };
+
+  const prevNextPage = value => {
+    dispatch(createAction(CHANGE_PAGE_TYPE.PREV_NEXT_SUCCESS, value));
+  };
   return (
     <Pagination>
-      <Pagination.Prev />
+      <Pagination.Prev onClick={() => prevNextPage(false)} />
       {renderPage()}
-      <Pagination.Next />
+      <Pagination.Next onClick={() => prevNextPage(true)} />
     </Pagination>
   );
 };
--- src/redux/Constants/Page.js
+++ src/redux/Constants/Page.js
@@ -1,6 +1,7 @@
 import createRequestTypes from "./request";
 
 const CHANGE_PAGE_TYPE = {
-  ...createRequestTypes("PAGE", "CHANGE_PAGE")
+  ...createRequestTypes("PAGE", "CHANGE_PAGE"),
+  ...createRequestTypes("PAGE", "PREV_NEXT"),
 };
 export default CHANGE_PAGE_TYPE;
